e76a73452611cea83edf3db394583b9b3d399c15
sanidhya004 <80572505+sanidhya004@users.noreply.github.com>
2023-01-31 18:36:40 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import TopBar from './Component/TopBar';
+import { FollowupPage } from './Component/FollowupPage';
+import  Carousel from './Component/Carousel';
+function App() {
+  return (
+    <div className="App">
+        
+         <FollowupPage name="Cakes" image="cake.png" id="1"  color="yellow"/>
+         
+         <FollowupPage name="Bento" image="pastry.png" id="2" color="white"/>
+         <FollowupPage name="Cupcake" image="cupcake.png" id="3" color="yellow"/>
+         
 
+    </div>
+  );
+}
+
+export default App;
